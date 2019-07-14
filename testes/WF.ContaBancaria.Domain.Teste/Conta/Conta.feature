2969e4e9511c235b2c3f059d6b80cc6c6310a254
williame <william_figueredo@hotmail.com>
2019-07-13 23:43:48 -0300

    Adicionando Specflow

---
+++ testes/WF.ContaBancaria.Domain.Teste/Conta/Conta.feature
@@ -0,0 +1,10 @@
+﻿Funcionalidade: Deposito em Conta Corrente
+				Operações de deposito em conta corrente de um cliente
+
+@TesteAceitacaoContaCorrente
+
+Cenario: Deposito Realizado com Sucesso
+Dado Uma Conta Corrente Ativa
+	Quando Um valor for depositado
+	E O valor e superior a zero
+	Então Receberei uma mensagem de deposito realizado com sucesso
---
+++ testes/WF.ContaBancaria.Domain.Teste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WF.ContaBancaria.Domain.Teste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WF.ContaBancaria.Domain.Teste")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("598c9a2d-9fb0-4a70-aa48-21769a02a73d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
