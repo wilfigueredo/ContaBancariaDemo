52f03e266aac2a8bfca62df6061356fd8369a1a1
wilfigueredo <william_figueredo@hotmail.com>
2019-07-31 14:17:26 -0300

    remove comentario

--- src/WF.ContaBancaria.Domain/Models/Entity.cs
+++ src/WF.ContaBancaria.Domain/Models/Entity.cs
@@ -8,8 +8,7 @@ namespace WF.ContaBancaria.Domain.Models
 {
     public abstract class Entity
     {
-        // foi adotado o uso do Guid ao invés do numeric
-        // o guid é gerado na instância do objeto e tem algumas vantagens em relação ao numeric        
+              
         protected Entity()
         {
             Id = Guid.NewGuid();
